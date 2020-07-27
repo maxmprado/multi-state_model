db61f3c897b81396d7cf52adca604e13099224ce
Beto <Beto@MacBook-Pro-de-Beto.local>
2020-07-27 00:17:32 -0500

    Avances 27 julio

--- data_cleaning_and_model.R
+++ data_cleaning_and_model.R
@@ -8,3 +8,13 @@ datos <- read.csv(unz(archivoTemporal,"historiavida.csv"))
 unlink(archivoTemporal)
 
 # Empezamos limpiando los datos
+# Seleccionamos sólo variables necesarias
+
+#1 sí, 2 no
+head(datos[c("folioviv","foliohog","id_pobla","anio_retro","anio_nac", "sexo","niv_aprob",
+             #"trabajo","pos_tra",
+             "edo_civil1","edo_civil2","edo_civil3","edo_civil4","edo_civil5","edo_civil6")])
+
+
+datos$
+# Extraemos de folioviv la residencia del individuo
