a40eee11d43bdb7c2fb0e9c767ba1218927f97e3
maxmprado <67024312+maxmprado@users.noreply.github.com>
2020-07-26 23:01:42 -0500

    Update data_cleaning_and_model.R

--- data_cleaning_and_model.R
+++ data_cleaning_and_model.R
@@ -1,1 +1,10 @@
 #modelo multi estado para las relaciones conyugales
+
+# Descargamos los datos directamente del INEGI
+archivoTemporal<- tempfile()
+zip <-  "https://www.inegi.org.mx/contenidos/programas/eder/2017/microdatos/eder2017_bases_csv.zip"
+download.file(zip,archivoTemporal)
+datos <- read.csv(unz(archivoTemporal,"historiavida.csv"))
+unlink(archivoTemporal)
+
+# Empezamos limpiando los datos
