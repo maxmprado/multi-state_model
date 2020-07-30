e83b933cd137ea4d8c6845ab2fcd51b75e85c5a9
Beto <Beto@MacBook-Pro-de-Beto.local>
2020-07-30 02:22:11 -0500

    Logramos remover todos los individuos cuyos estados no eran de interés para nuestro estudio

--- data_cleaning_and_model.R
+++ data_cleaning_and_model.R
@@ -9,21 +9,54 @@ datos <- read.csv(unz(archivoTemporal,"historiavida.csv"))
 unlink(archivoTemporal)
 
 # Empezamos limpiando los datos
+
+
+# Crearemos la variable id concatenando 3 variables 
+# Creamos la variable residencia del individuo
+# Esta información la extraemos de folioviv 
+datos <- datos %>% as_tibble()  %>% mutate(id = paste0(datos$folioviv,datos$foliohog,datos$id_pobla),
+                                           resid = ifelse(substr(datos$folioviv,3,3)=="6","Rural","Urbana")
+                                           )
+
 # Seleccionamos sólo variables necesarias
-#1 sí, 2 no
+datos <- datos %>% select(id, anio_retro, anio_nac, resid, sexo,
+                          #trabajo, pos_tra,
+                          niv_aprob,starts_with("edo_civil"))
 
-datos$ID <- paste0(datos$folioviv,datos$foliohog,datos$id_pobla) #mutate
-# Extraemos de folioviv la residencia del individuo
-datos$residencia <- ifelse(substr(datos$folioviv,3,3)=="6","Rural","Urbana")  #mutate
-datos<-datos[c("ID","anio_retro","anio_nac", "residencia","sexo","niv_aprob",
-             #"trabajo","pos_tra",
-             "edo_civil1","edo_civil2","edo_civil3","edo_civil4","edo_civil5","edo_civil6")] #select
-View(datos)
 
-#Nos interesan únicamente los años en que hubo cambio de estado
-#No podemos estudiar a los individuos que tuvieron cambio de estado en el mismo año
 
-unique(datos$ID)
+
+# De esta tabla auxiliar extraeremos los estados
+# que no son de nuestro interés
+y <- datos %>% mutate_at(.vars = vars(edo_civil1:edo_civil6),
+            .funs = list(~ ifelse((.>10 & .%%10 != 0) ,(.*0+100),.)))
+
+# Removeremos los ID de las personas cuyos estados
+# no son de interés para este análisis
+remover <-  unique(y %>% filter(
+            edo_civil1==100 |
+            edo_civil2==100 |
+            edo_civil3==100 |
+            edo_civil4==100 |
+            edo_civil5==100 |
+            edo_civil6==100 ) %>% select(id))
+
+# length(unique(datos$id)) #23831
+
+532/23831 #eliminado entre muestra total
+
+# eliminaremos 532 personas de la muestra
+# lo que representa una proporción menor al
+# 3% del total de personas en observación
+# por lo tanto no afecta significativamente
+# la población original.
+
+datos <- datos %>% anti_join(remover)
+
+
+# Nos interesan únicamente los años en 
+# que hubo cambio de estado
+
 # Reacomodamos los datos en un nuevo data frame de manera que tengamos toda la historia
 # En una sola columna
 View(subset(datos,edo_civil1<10 & edo_civil2<10 & edo_civil3 <10 ))
