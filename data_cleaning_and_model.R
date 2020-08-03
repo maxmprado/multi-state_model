74fe05632ad7acb92c45886ecf4c6ef1ae441411
Beto <Beto@MacBook-Pro-de-Beto.local>
2020-08-03 01:34:13 -0500

    Datos corregidos nuevamento proximo a preparar para el análsis multi estado.

--- data_cleaning_and_model.R
+++ data_cleaning_and_model.R
@@ -1,5 +1,5 @@
-library(dplyr)
-library(tidyr)
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(tidyr))
 #modelo multi estado para las relaciones conyugales
 
 # Descargamos los datos directamente del INEGI
@@ -29,57 +29,63 @@ datos <- datos %>% select(id, anio_retro, anio_nac, resid, sexo,
 
 # De esta tabla auxiliar extraeremos los estados
 # que no son de nuestro interés
-y <- datos %>% mutate_at(.vars = vars(edo_civil1:edo_civil6),
-            .funs = list(~ ifelse((.>10 & .%%10 != 0) ,(.*0+100),.)))
-
 # Removeremos los ID de las personas cuyos estados
 # no son de interés para este análisis
-remover <-  unique(y %>% filter(
-            edo_civil1==100 |
-            edo_civil2==100 |
-            edo_civil3==100 |
-            edo_civil4==100 |
-            edo_civil5==100 |
-            edo_civil6==100 ) %>% select(id))
 
-# length(unique(datos$id)) #23831
+remover1 <- (datos %>% mutate_at(.vars = vars(edo_civil1:edo_civil6),
+            .funs = list(~ ifelse((.>10 & .%%10 != 0) ,(.*0+100),.))) %>% 
+    filter(
+    edo_civil1==100 |
+    edo_civil2==100 |
+    edo_civil3==100 |
+    edo_civil4==100 |
+    edo_civil5==100 |
+    edo_civil6==100 ) %>% select(id) %>% distinct())
 
-532/23831 #eliminado entre muestra total
 
-# eliminaremos 532 personas de la muestra
+datos <- datos %>% anti_join(remover1)
+
+# Nos interesan únicamente los años en 
+# que hubo cambio de estado
+
+# Ahora vamos a eliminar a los individuos que tuvieron
+# varias parejas al mismo tiempo
+
+edos_rep<-c(1:4,seq(10,40, by=10))
+
+remover2 <- (unique(datos %>% filter((edo_civil1 %in% edos_rep & edo_civil2 %in% edos_rep)|
+                 (edo_civil1 %in% edos_rep & edo_civil3 %in% edos_rep)|
+                 (edo_civil1 %in% edos_rep & edo_civil4 %in% edos_rep)|
+                 (edo_civil2 %in% edos_rep & edo_civil3 %in% edos_rep)|
+                 (edo_civil2 %in% edos_rep & edo_civil4 %in% edos_rep)
+                 ) %>% select(id)))
+
+
+datos <- datos %>% anti_join(remover2)
+
+# En total eliminamos 631 personas de la muestra
+(532+99)/23831 
 # lo que representa una proporción menor al
 # 3% del total de personas en observación
 # por lo tanto no afecta significativamente
 # la población original.
 
-datos <- datos %>% anti_join(remover)
-
-# Nos interesan únicamente los años en 
-# que hubo cambio de estado
-View(datos)
 
 # Reacomodamos los datos en un nuevo data frame de manera que tengamos toda la historia
 # En una sola columna
+dd <- (rbind(datos %>% select(1:6,edo=edo_civil1),
+             datos %>% select(1:6,edo=edo_civil2),
+             datos %>% select(1:6,edo=edo_civil3),
+             datos %>% select(1:6,edo=edo_civil4),
+             datos %>% select(1:6,edo=edo_civil5),
+             datos %>% select(1:6,edo=edo_civil6)
+              )
+              %>%distinct()%>% arrange(id, anio_retro)
+              %>% mutate(edo = ifelse(anio_retro!=anio_nac & edo == 0,100,edo))
+              %>%filter(edo<10) 
+      )
 
 
-
-dd<-(rbind(datos %>% select(1:6,edo=edo_civil1),
-           datos %>% select(1:6,edo=edo_civil2),
-           datos %>% select(1:6,edo=edo_civil3),
-           datos %>% select(1:6,edo=edo_civil4),
-           datos %>% select(1:6,edo=edo_civil5),
-           datos %>% select(1:6,edo=edo_civil6)
-           ) %>% arrange(id, anio_retro) %>% distinct() 
-             %>% group_by(id,anio_retro,anio_nac,resid,sexo,niv_aprob)
-             %>% summarise(edo=max(edo))
-             %>% filter(edo<10)
-             %>% group_by(id,anio_nac,resid,sexo,niv_aprob,edo)
-             %>% summarise(anio_retro = min(anio_retro))
-             %>% select(id, anio_retro,anio_nac,resid,sexo,niv_aprob, edo)
-     )
-
-dd<-as.data.frame(dd)
-unique(dd$edo) 
 
 dd <- dd %>% mutate(id=as.factor(id),
               resid = as.factor(resid),
@@ -87,12 +93,13 @@ dd <- dd %>% mutate(id=as.factor(id),
               edo = as.factor(edo),
               niv_aprob = ifelse(is.na(niv_aprob),0,niv_aprob))
 
+270152210011
 
-str(dd)
 # Convertimos a formato largo para poder prepara los datos
 # con mstate.
 ddd <- dd %>% spread(key = c("edo"), value = c("anio_retro"))
 
+View(ddd)
 
 
 dd <- (dd %>% mutate(edo = case_when(between(edo,2,4)~"M",
